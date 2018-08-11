19d9804641cff510d4730db5ae1242925648368b
Jakob Borg <jakob@kastelo.net>
2018-08-11 10:30:17 +0200

    wip

--- lib/model/folder_recvonly_test.go
+++ lib/model/folder_recvonly_test.go
@@ -22,17 +22,35 @@ func TestRecvOnlyRevertDeletes(t *testing.T) {
 	// Make sure that we delete extraneous files and directories when we hit
 	// Revert.
 
-	os.RemoveAll("_recvonly")
-	defer os.RemoveAll("_recvonly")
+	if err := os.RemoveAll("_recvonly"); err != nil && !os.IsNotExist(err) {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := os.RemoveAll("_recvonly"); err != nil && !os.IsNotExist(err) {
+			t.Fatal(err)
+		}
+	}()
 
 	// Create some test data
 
-	os.MkdirAll("_recvonly/.stfolder", 0755)
-	os.MkdirAll("_recvonly/ignDir", 0755)
-	os.MkdirAll("_recvonly/unknownDir", 0755)
-	ioutil.WriteFile("_recvonly/ignDir/ignFile", []byte("hello\n"), 0644)
-	ioutil.WriteFile("_recvonly/unknownDir/unknownFile", []byte("hello\n"), 0644)
-	ioutil.WriteFile("_recvonly/.stignore", []byte("ignDir\n"), 0644)
+	if err := os.MkdirAll("_recvonly/.stfolder", 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.MkdirAll("_recvonly/ignDir", 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.MkdirAll("_recvonly/unknownDir", 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile("_recvonly/ignDir/ignFile", []byte("hello\n"), 0644); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile("_recvonly/unknownDir/unknownFile", []byte("hello\n"), 0644); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile("_recvonly/.stignore", []byte("ignDir\n"), 0644); err != nil {
+		t.Fatal(err)
+	}
 
 	knownFiles := setupKnownFiles(t, []byte("hello\n"))
 
@@ -107,12 +125,20 @@ func TestRecvOnlyRevertNeeds(t *testing.T) {
 	// Make sure that a new file gets picked up and considered latest, then
 	// gets considered old when we hit Revert.
 
-	os.RemoveAll("_recvonly")
-	defer os.RemoveAll("_recvonly")
+	if err := os.RemoveAll("_recvonly"); err != nil && !os.IsNotExist(err) {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := os.RemoveAll("_recvonly"); err != nil && !os.IsNotExist(err) {
+			t.Fatal(err)
+		}
+	}()
 
 	// Create some test data
 
-	os.MkdirAll("_recvonly/.stfolder", 0755)
+	if err := os.MkdirAll("_recvonly/.stfolder", 0755); err != nil {
+		t.Fatal(err)
+	}
 	oldData := []byte("hello\n")
 	knownFiles := setupKnownFiles(t, oldData)
 
